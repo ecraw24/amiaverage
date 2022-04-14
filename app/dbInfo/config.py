6b32ab8a9d1fe5cfee4a2b29a9b4a1fb90eeaaee
ecraw24 <64031478+ecraw24@users.noreply.github.com>
2022-04-13 19:30:17 -0600

    Delete app/dbInfo directory

--- app/dbInfo/config.py
+++
@@ -1,6 +0,0 @@
-#Database Credentials
-host = ec2-3-217-251-77.compute-1.amazonaws.com
-port = 5432
-database = d4ecfjhp2gbtco
-user = nvalcjgkjirosy
-password = a1d078c1ef28b3ccb5d5f9ff4583e42243fbd419766d05dacafd70e3dbd79d62
