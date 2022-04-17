2d9cf79459f1557ad128e99578ed51c9d1c64532
Jeffrey Mason <mason.m.jeffrey@gmail.com>
2022-04-16 18:09:13 -0600

    added examples of getting variables

--- db_data.py
+++ db_data.py
@@ -7,6 +7,14 @@ conn_info = subprocess.run(["heroku", "config:get", "DATABASE_URL", "-a", HEROKU
 connuri = conn_info.stdout.decode('utf-8').strip()
 conn = psycopg2.connect(connuri)
 cursor = conn.cursor()
-cursor.execute(sql.SQL("SELECT *  FROM {};").format(sql.Identifier(TABLE_NAME)))
-count = cursor.fetchall()
-print(count)
+
+#cursor.execute(sql.SQL("SELECT *  FROM {};").format(sql.Identifier(TABLE_NAME)))
+cursor.execute(sql.SQL("SELECT Skill_name FROM {};").format(sql.Identifier(TABLE_NAME)))
+categories = cursor.fetchall()
+cursor.execute(sql.SQL("SELECT Skill_verb FROM {};").format(sql.Identifier(TABLE_NAME)))
+skillverbs = cursor.fetchall()
+
+
+#count = cursor.fetchall()
+print(categories)
+print(skillverbs)
