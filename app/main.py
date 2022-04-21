78a0c2b6abbdae21c70d704645127d841d6419f4
Kevin Vick <kevi9621@colorado.edu>
2022-04-21 01:18:10 -0600

    indentation error correction in results plot

--- app/main.py
+++ app/main.py
@@ -205,16 +205,16 @@ def get_percentile(score, list):
     return (place * 20) + (score // (next_lvl - prev_lvl))
 
 def plot():
-        img = BytesIO()
-        y = [1,2,3,4,5]
-        x = [0,2,1,3,4]
+    img = BytesIO()
+    y = [1,2,3,4,5]
+    x = [0,2,1,3,4]
 
-        plt.plot(x,y)
+    plt.plot(x,y)
 
-        plt.savefig(img, format='png')
-        plt.close()
-        img.seek(0)
-        plot_url = base64.b64encode(img.getvalue()).decode('utf8')
-
+    plt.savefig(img, format='png')
+    plt.close()
+    img.seek(0)
+    plot_url = base64.b64encode(img.getvalue()).decode('utf8')
+    
     return plot_url
     
