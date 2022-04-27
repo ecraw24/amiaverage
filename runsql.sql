106e499c1b6f212ba8d9457cf0dd730ae8fd418b
Jeffrey Mason <mason.m.jeffrey@gmail.com>
2022-04-27 17:04:26 -0600

    just some sql changes, added pictures and changed one minute mile to weekly miles

--- runsql.sql
+++ runsql.sql
@@ -47,12 +47,12 @@ CREATE TABLE skillsinfo
 --VALUES ('5', 'Run a Mile', 'run a', 'minute mile', 'http://cdn.mos.cms.futurecdn.net/v44n2mBJgaRoCkkFGjDtRP.jpeg', 'Running is the activity of moving fast on foot, especially as a sport. The 1-mile run is a common measurement of aerobic fitness.');
 -- inserts for skillsinfo
 INSERT INTO skillsinfo (id, skill_name, skill_verb, skill_metric, unit_of_measurement, picture, descrip, level1, level2, level3, level4, level5)
-VALUES ('1', 'Fishing', 'catch',  'fish', 'pounds', '', 'Fishing is the activity of using a rod and bait to catch aquatic life.', '5', '10', '15', '20', '25');
+VALUES ('1', 'Fishing', 'catch',  'fish', 'pounds', 'https://www.takemefishing.org/getmedia/02981128-b7e1-4ea0-adbd-75bc5b2c7ba0/homehero_woman.jpg', 'Fishing is the activity of using a rod and bait to catch aquatic life.', '5', '10', '15', '20', '25');
 INSERT INTO skillsinfo (id, skill_name, skill_verb, skill_metric, unit_of_measurement, picture, descrip, level1, level2, level3, level4, level5)
-VALUES ('2', 'Hot Dog Eating', 'eat', 'hot dogs in an hour', 'Hot Dogs', '', 'Hot Dog Eating is a competitive sport in which people try to consume as many hot dogs as possible.', '1', '3', '6', '9', '12');
+VALUES ('2', 'Hot Dog Eating', 'eat', 'hot dogs in an hour', 'Hot Dogs', 'https://www.denverpost.com/wp-content/uploads/2018/07/14a8dd5c2499465e933a5bb07c1c2704.jpg', 'Hot Dog Eating is a competitive sport in which people try to consume as many hot dogs as possible.', '1', '3', '6', '9', '12');
 INSERT INTO skillsinfo (id, skill_name, skill_verb, skill_metric, unit_of_measurement, picture, descrip, level1, level2, level3, level4, level5)
-VALUES ('3', 'IQ', 'perform tests for a', 'IQ', 'Points', '', 'Intelligence Quotient (IQ) is a standard measure of intellect.', '85', '115', '130', '145', '159');
+VALUES ('3', 'IQ', 'perform tests for a', 'IQ', 'Points', 'https://caltech-prod.s3.amazonaws.com/main/images/CT_Quartz-IQ_SPOTLIGHT.original.jpg', 'Intelligence Quotient (IQ) is a standard measure of intellect.', '85', '115', '130', '145', '159');
 INSERT INTO skillsinfo (id, skill_name, skill_verb, skill_metric, unit_of_measurement, picture, descrip, level1, level2, level3, level4, level5)
 VALUES ('4', 'Bench Press', 'bench', 'lbs for 3 reps', 'Pounds', 'https://cdn2.picryl.com/photo/2011/06/04/hiroko-yanai-bench-presses-99-pounds-during-the-2011-36d1e9-1600.jpg', 'A bench press is a compound a bodybuilding and weightlifting exercise in which a lifter lies on a bench with the feet on the floor and raises a weight with both arms.', '70', '110', '170', '220', '250');
 INSERT INTO skillsinfo (id, skill_name, skill_verb, skill_metric, unit_of_measurement, picture, descrip, level1, level2, level3, level4, level5)
-VALUES ('5', 'Run a Mile', 'run a', 'minute mile', 'Minutes', 'http://cdn.mos.cms.futurecdn.net/v44n2mBJgaRoCkkFGjDtRP.jpeg', 'Running is the activity of moving fast on foot, especially as a sport. The 1-mile run is a common measurement of aerobic fitness.', '12','10','8','6','5');
+VALUES ('5', 'Miles Run in a Week', 'Run', 'Weekly Miles', 'Miles', 'http://cdn.mos.cms.futurecdn.net/v44n2mBJgaRoCkkFGjDtRP.jpeg', 'Running is the activity of moving fast on foot, especially as a sport. Running throughout the week is an important part of aerobic fitness.', '2','5','10','20','26');
